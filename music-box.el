d33823c0e6c980ca7a01b180ca371d1938aa7dea
writecoffee <writecoffee@gmail.com>
2015-05-18 21:20:38 -0700

    Interative elastic search with helm

--- music-box.el
+++ music-box.el
@@ -1,5 +1,6 @@
 (require 'json)
 (require 'helm)
+(require 'multi)
 
 (defun encrypt-id (id)
   (lexical-let* ((magic (vconcat "3go8&$8*3*3h0k(2)2"))
@@ -33,12 +34,12 @@
                                         "-X POST "
                                         "\""
                                         "http://music.163.com/api/search/get/?"
-                                        "limit=20"
-                                        "&s="
-                                        MACRO-SONG-QUERY
+                                        "limit=2"
                                         "&type=1"
                                         "&sub=false"
                                         "&offset=0"
+                                        "&s="
+                                        MACRO-SONG-QUERY
                                         "\""))
 
 (defconst CMD-CURL-GET-SONG-DETAIL (concat "curl -s "
@@ -63,40 +64,54 @@
                               CMD-CURL-GET-SONG-DETAIL))))
 
 (defun cmd-search-songs (search-query)
-  (let ((formatted-query-words search-query))
+  (let ((formatted-query-words (url-hexify-string search-query)))
     (json-read-from-string
      (shell-command-to-string
       (replace-regexp-in-string MACRO-SONG-QUERY
                                 formatted-query-words
                                 CMD-CURL-SEARCH-SONGS)))))
 
-(defun helm-music-163-search (search-query)
+(defun helm-netease-music-search ()
   (let* ((lambda-format-for-display (lambda (song)
                                       (cdr (assoc 'name song))))
-         (lambda-get-song-download-url (lambda (song)
-                                         (let ((song-detail (cmd-get-song-detail
-                                                             (number-to-string (cdr (assoc 'id song))))))
-                                           (cdr (assoc 'mp3Url
-                                                       (elt (cdr (assoc 'songs song-detail))
-                                                            0))))))
-         (json-response (cmd-search-songs search-query))
-         (song-list (cdr (assoc 'songs (assoc 'result json-response)))))
+         (json-response (cmd-search-songs helm-pattern))
+         (song-list (cdr (assoc 'songs (assoc 'result json-response))))
+         (result-list
+          (mapcar (lambda (song)
+                    (cons (funcall lambda-format-for-display song)
+                          (number-to-string (cdr (assoc 'id song)))))
+                  song-list)))
+    result-list))
 
-    (mapcar (lambda (song)
-              (cons (funcall lambda-format-for-display song)
-                    (funcall lambda-get-song-download-url song)))
-            song-list)))
+(defun helm-netease-music-play-song (song-url)
+  (let ((lambda-get-song-download-url
+          (lambda ()
+            (let ((song-detail (cmd-get-song-detail song-url)))
+              (cdr (assoc 'mp3Url
+                          (elt (cdr (assoc 'songs song-detail))
+                               0)))))))
 
-(defvar musics (helm-music-163-search "lady+gaga"))
+    (async-shell-command
+     (concat "mpg123 "
+             (funcall lambda-get-song-download-url))
+     MUSIC-PLAYER-BUFFER-NAME)))
 
-(defun helm-music-163-play-song (song-url)
-  (async-shell-command (concat "mpg123 "
-                               song-url)
-                       MUSIC-PLAYER-BUFFER-NAME))
-
-(defvar helm-sources-music-163
+(defvar helm-sources-netease-music
   '((name . "NetEase Music")
-    (candidates . musics)
-    (action . (("Play track" . helm-music-163-play-song)))))
+    (volatile)
+    (delayed)
+    (multiline)
+    (requires-pattern . 2)
+    (candidates-process . helm-netease-music-search)
+    (action . (("Play track" . helm-netease-music-play-song)))))
 
-(helm :sources '(helm-sources-music-163))
+(defun helm-netease-music ()
+  "Netease music search interface in helm."
+  (interactive)
+  (let ((back-helm-input-idle-delay helm-input-idle-delay))
+    (setq helm-input-idle-delay 1)
+    (helm :sources '(helm-sources-netease-music)
+          :buffer "*helm-netease-music*")
+    (setq helm-input-idle-delay back-helm-input-idle-delay)))
+
+(helm-netease-music)
